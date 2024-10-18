e22ce2f1ba29baf1ae596b37f8aaad445ace6c5c
danewhitfield <danejwhitfield@gmail.com>
2024-10-18 17:40:05 +0100

    added a new branch

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,12 @@
 export interface UserChoices {
-	typescript: boolean;
-	tailwind: boolean;
-	router: boolean;
-	axios: boolean;
-	shadcn: boolean;
-	vitest: boolean;
+  typescript: boolean;
+  tailwind: boolean;
+  router: boolean;
+  axios: boolean;
+  shadcn: boolean;
+  vitest: boolean;
+}
+
+export interface NewType {
+  name: string;
 }
